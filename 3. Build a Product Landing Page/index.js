ec10c9c1653b083ed154b7d57cfabafdd69fa1b2
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2021-09-18 20:06:25 +0530

    Add files via upload

---
+++ 3. Build a Product Landing Page/index.js
@@ -0,0 +1,4 @@
+// coded by @geekblower 18 September 2021
+const projectName = 'product-landing-page';
+localStorage.setItem('example_project', 'Product Landing Page');
+// REPLACE CDN WITH GITCDN!!!
